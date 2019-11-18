94a56c780e62efd49e8056b3a31da797f56d5778
shreayan98c <shreayan98c@gmail.com>
2019-11-19 00:20:33 +0530

    Creating the redirect page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/universities')
+def univ():
+	return render_template('univ.html')
+
 # @app.route('/results')
 
 if __name__ == '__main__':
