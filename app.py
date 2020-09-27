23d3074329543f7a32799fc22af5240bc25cd8c4
shreayan98c <shreayan98c@gmail.com>
2020-09-27 13:29:25 +0530

    Fixed jinja templating

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/universities')
+@app.route('/universities', methods=['POST'])
 def universities():
 	return render_template('univ.html')
 
