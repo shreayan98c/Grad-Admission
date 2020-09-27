38978da23d614685aec70d7a5be102905bbf0f8e
shreayan98c <shreayan98c@gmail.com>
2020-09-27 14:33:08 +0530

    Reading values from form

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+import pickle
 
 app = Flask(__name__)
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def index():
@@ -8,7 +11,20 @@ def index():
 
 @app.route('/universities', methods=['POST'])
 def universities():
-	return render_template('univ.html')
+	if request.method == 'POST':
+		gre = request.form['gre']
+		toefl = request.form['toefl']
+		rating = request.form['gridRadios']
+		sop = request.form['sop']
+		lor = request.form['lor']
+		cgpa = request.form['cgpa']
+		research = request.form.getlist('research')
+		if(not research):
+			research=0
+		else:
+			research=1
+		
+		# return render_template('univ.html', result=result)
 
 # @app.route('/results')
 
